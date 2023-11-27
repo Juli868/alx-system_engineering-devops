9bc1e89a67435401f4ddced75ff962b700b3a142
juli868 <bugijul@gmail.com>
2023-11-27 22:32:15 +0200

    First tsk

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
-  file { '/tmp/school':
-    ensure  => 'present',
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0744',
-    content => 'I love Puppet'
-  }
+#Creating a file that contains ILOVE PUPPET
+file { '/tmp/school':
+  ensure  => 'present',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet'
+}
