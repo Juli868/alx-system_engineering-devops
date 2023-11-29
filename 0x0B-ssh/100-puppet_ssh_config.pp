bec9afc4d77fea4ebd0e27d8f589344782f8e50d
juli868 <bugijul@gmail.com>
2023-11-30 00:18:33 +0200

    some touches

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,11 @@
 #adding a user to my ssh configuration file
 file {'/etc/ssh/ssh_config':
   ensure  => present,
-  content => "Host server 02\n  HostName 54.160.86.207\n  User ubuntu\n  IdentityFile ~/.ssh/school",
+  content => "Host server 02
+  HostName 54.160.86.207
+User ubuntu
+IdentityFile ~/.ssh/school
+PasswordAuthentication no",
   mode    => '0644',
   unless  => "grep -q 'Host server 02\n  HostName 54.160.86.207\n  User ubuntu\n  IdentityFile ~/.ssh/school' ~/.ssh/config",
 }
