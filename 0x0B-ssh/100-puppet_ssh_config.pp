9b4c4ab1477832c96ad8d66f7e776831b44e4a6d
juli868 <bugijul@gmail.com>
2023-11-30 00:20:03 +0200

    Editing the condition

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -6,6 +6,6 @@ file {'/etc/ssh/ssh_config':
 User ubuntu
 IdentityFile ~/.ssh/school
 PasswordAuthentication no",
-  mode    => '0644',
-  unless  => "grep -q 'Host server 02\n  HostName 54.160.86.207\n  User ubuntu\n  IdentityFile ~/.ssh/school' ~/.ssh/config",
+  mode    => '0600',
+  unless  => "grep -q 'Host server 02\n  HostName 54.160.86.207\n  User ubuntu\n  IdentityFile ~/.ssh/school' /etc/ssh/ssh_config",
 }
