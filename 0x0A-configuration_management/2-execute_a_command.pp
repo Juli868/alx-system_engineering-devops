d45bd493e787860a82f1cb2101d85ea2a49d7d93
juli868 <bugijul@gmail.com>
2023-11-30 01:07:09 +0200

    Third task re do

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 #killing a process
 exec {'Killing a process':
-  command     => 'pkill -9 -f killmenow',
-  path        => ['/usr/bin', '/usr/sbin']
+  command => 'pkill -9 -f killmenow',
+  path    => ['/usr/bin', '/usr/sbin']
 }
