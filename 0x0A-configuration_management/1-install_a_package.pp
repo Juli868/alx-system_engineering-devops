9c296f3680677febda4d9193b9d200eb7105d065
juli868 <bugijul@gmail.com>
2023-12-05 21:56:06 +0200

    Using command approach

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
 #installing flask using puppet
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip',
+command {'install_flask_with_pip':
+  command => 'pip3 install flask=2.1.0',
+  path    => '/usr/bin/pip3',
+  user    => 'root',
+  onlyif  => 'pip3 --version'
 }
