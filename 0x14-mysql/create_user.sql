00eecd1d3f4111a7412dcd1e4fb0b9c65450c5fe
juli868 <bugijul@gmail.com>
2023-12-19 18:49:22 +0200

    Granting the user

--- 0x14-mysql/create_user.sql
+++ 0x14-mysql/create_user.sql
@@ -1,3 +1,3 @@
 -- creating a user
 CREATE USER IF NOT EXISTS 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'holberton_user'@'localhost';
